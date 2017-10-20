6027dadfbdeca4a9afbda637da7cc16423170fdc
Yeonjin Noh <nyg0813@gmail.com>
2017-10-20 22:20:36 +0900

    Git error is made

---
+++ hello.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	if ((pid = fork()) < 0) {
+		fprintf(stderr, "fork error\n");
+	} else if (pid == 0) {  /* specify pathname, specify environment */
+		execl( "/bin/ls", "/bin/ls", NULL); 
+		printf("I’m Child PID = %d, Parent PID =%d\n", getpid(), getppid());	
+		
+	}
+
+	if (waitpid(pid, NULL, 0) < 0)
+		fprintf(stderr, "wait error\n");
+
+	if ((pid = fork()) < 0) {
+		fprintf(stderr, "fork error\n");
+	} else if (pid == 0) {  /* specify filename, inherit environment */
+		system("/bin/ls -a"); 
+		printf("I’m Child PID = %d, Parent PID =%d\n", getpid(), getppid());	
+	}
+	exit(0); 
+}
+
